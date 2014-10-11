f27a524651151836ef73c643370c5c411cf204ff
janos <janos.jarecsni@gmail.com>
2014-10-11 15:09:35 +0100

    Third part of the tutorial implemented

--- requirejs-tutorial/js/app.js
+++ requirejs-tutorial/js/app.js
@@ -2,6 +2,13 @@
  * Created by janos on 11/10/2014.
  */
 define(function (require) {
-    var alerter = require("alerter");
-    alerter("Hello from the app");
-})
+    var button = document.createElement("button");
+    button.onclick = function ()
+    {
+        require(["alerter"], function(alerter) {
+            alerter("Hello from the app");
+        });
+    };
+    button.textContent = "Click me to load alerter.";
+    document.body.appendChild(button);
+});
